db0dc4c882e4cba5f59378820bf6d1e8eab11fa9
Unknown <22mishrt@elmbrookstudents.org>
2019-07-18 21:41:13 -0500

    Age Decision Class

---
+++ src/summer2019/AgeDecision.java
@@ -0,0 +1,20 @@
+package summer2019;
+
+import java.util.Scanner;
+
+public class AgeDecision {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your age.");
+		int age = input.nextInt();
+		System.out.println("I hope you have a great day?");
+		if (age <= 17) {
+			System.out.println("You're getting an A in this class");
+		}
+			
+		
+		
+	}
+}
+//thank you eclipse
